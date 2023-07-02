cebceca468fc787d1dec87169357cc8aa339e90a
Henadzi Shutko <hazardsoft@gmail.com>
2023-07-02 11:22:08 +0300

    test: implement fs and fs/promises mocking

--- src/06-mocking-node-api/index.test.ts
+++ src/06-mocking-node-api/index.test.ts
@@ -1,5 +1,22 @@
-// Uncomment the code below and write your tests
-// import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
+import { readFileAsynchronously } from '.';
+import path from 'path';
+import fs from 'fs';
+import fsPromises from 'fs/promises';
+
+const pathToFile = 'file1.txt';
+const fileBuffer = Buffer.from('file content', 'utf-8');
+
+jest.mock('fs', () => {
+  return {
+    existsSync: jest.fn(),
+  };
+});
+
+jest.mock('fs/promises', () => {
+  return {
+    readFile: jest.fn(),
+  };
+});
 
 describe('doStuffByTimeout', () => {
   beforeAll(() => {
@@ -38,15 +55,39 @@ describe('doStuffByInterval', () => {
 });
 
 describe('readFileAsynchronously', () => {
+  afterAll(() => {
+    jest.unmock('fs');
+    jest.unmock('fs/promises');
+  });
+
   test('should call join with pathToFile', async () => {
-    // Write your test here
+    const mockedPathJoin = jest.spyOn(path, 'join');
+
+    await readFileAsynchronously(pathToFile);
+    expect(mockedPathJoin.mock.lastCall).toContainEqual(pathToFile);
+    expect(mockedPathJoin).toHaveBeenCalled();
+    expect(mockedPathJoin).toHaveBeenCalledTimes(1);
+
+    mockedPathJoin.mockRestore();
   });
 
   test('should return null if file does not exist', async () => {
-    // Write your test here
+    fs.existsSync = jest.fn(() => false);
+
+    const fileContent: string | null = await readFileAsynchronously(pathToFile);
+    expect(fileContent).toBeNull();
+    expect(fs.existsSync).toHaveBeenCalledTimes(1);
   });
 
   test('should return file content if file exists', async () => {
-    // Write your test here
+    fs.existsSync = jest.fn(() => true);
+
+    fsPromises.readFile = jest.fn().mockResolvedValue(fileBuffer);
+    const fileContent: string | null = await readFileAsynchronously(pathToFile);
+
+    expect(fileContent).not.toBeNull();
+    expect(fileContent).toBe(fileBuffer.toString());
+    expect(fs.existsSync).toHaveBeenCalledTimes(1);
+    expect(fsPromises.readFile).toHaveBeenCalledTimes(1);
   });
 });
