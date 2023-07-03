26c217708311818f5e86043e6a498e3416621c48
Henadzi Shutko <hazardsoft@gmail.com>
2023-07-03 14:42:40 +0300

    fix: remove redundant table tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -3,8 +3,6 @@ import { simpleCalculator, Action } from './index';
 
 const testCases = [
   { a: 1, b: 2, action: Action.Add, expected: 3 },
-  { a: 2, b: 2, action: Action.Add, expected: 4 },
-  { a: 3, b: 2, action: Action.Add, expected: 5 },
   { a: 1, b: 2, action: Action.Subtract, expected: -1 },
   { a: 2, b: 3, action: Action.Multiply, expected: 6 },
   { a: 6, b: 3, action: Action.Divide, expected: 2 },
